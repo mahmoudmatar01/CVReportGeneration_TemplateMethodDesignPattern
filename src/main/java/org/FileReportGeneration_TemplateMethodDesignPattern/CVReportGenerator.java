975f38f27470b5e18dcd7dfcd9d6a47d54bd8244
mahmoudmatar <mahmoudmatar49@gmail.com>
2024-10-27 19:11:04 +0300

    implement template design pattern

--- src/main/java/org/FileReportGeneration_TemplateMethodDesignPattern/CVReportGenerator.java
+++ src/main/java/org/FileReportGeneration_TemplateMethodDesignPattern/CVReportGenerator.java
@@ -8,17 +8,17 @@ public abstract class CVReportGenerator {
     protected abstract GeneratedReport generateFinalReport(AnalyzedData analyzedData);
 
     // Same template implementation for all concrete classes
-    public File readCVFile(String cvFilePath) {
+    private File readCVFile(String cvFilePath) {
         System.out.println("Reading file from : "+cvFilePath);
         return new File(cvFilePath);
     }
 
-    public AnalyzedData analysisExtractData(ExtractedData extractedData) {
+    private AnalyzedData analysisExtractData(ExtractedData extractedData) {
         System.out.println("Analysis cv extracted data : "+extractedData.extractedData);
         return new AnalyzedData("Analysis cv extracted data : "+extractedData.extractedData);
     }
 
-    protected GeneratedReport generateCVReport(String cvFilePath){
+    public GeneratedReport generateCVReport(String cvFilePath){
         File cvFile = readCVFile(cvFilePath);
         ExtractedData extractDataFromCv= extractDataFromCv(cvFile);
         AnalyzedData analyzedData = analysisExtractData(extractDataFromCv);
